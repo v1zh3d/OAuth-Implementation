10e02b2cbf39b18a6ce9ae5dee6f857ca7e5d8ea
Vishweswar53 <vishweswar53@gmail.com>
2020-03-12 14:50:11 +0530

    added .gitignore

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -3,7 +3,9 @@ const GoogleStrategy = require('passport-google-oauth20');
 
 passport.use(
     new GoogleStrategy({
-    //options for strategy
+        //options for strategy
+        clientID: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET
     }), () => {
         //passport callback function
     }
