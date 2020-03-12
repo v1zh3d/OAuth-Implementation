b9a99ab2fd5ca41cf277352f30374c976b66335f
Vishweswar53 <vishweswar53@gmail.com>
2020-03-12 19:54:29 +0530

    conncted oauth

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -5,8 +5,9 @@ passport.use(
     new GoogleStrategy({
         //options for strategy
         clientID: process.env.GOOGLE_CLIENT_ID,
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET
-    }), () => {
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        callbackURL: '/auth/google/oauth2callback'
+    }, () => {
         //passport callback function
-    }
+    })
 );
--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const passport = require('passport');
 
 //auth-login
 router.get('/login', (req, res) => {
@@ -6,15 +7,19 @@ router.get('/login', (req, res) => {
 });
 
 //auth-with-google
-router.get('/google', (req, res) => {
-    //handle with PassportJS
-    res.send('Logging in with Google!');
-});
+router.get('/google', passport.authenticate('google', {
+    scope: ['profile', 'email']
+}));
 
 //auth-logout
 router.get('/logout', (req, res) => {
     //handle with PasspotyJS
     res.send('Logging out!');
+});
+
+//callback-route-for-google-2-redirect
+router.get('/google/oauth2callback', (req, res) => {
+    res.send('You reached the callback URI!');
 });
 
 module.exports = router;
